483732b2f9b5b9fc11319477ae08f091f71cf601
Seyyed Mehdi Mousavi <mehdimasiha396@gmail.com>
2026-09-24 20:50:21 +0330

    Add files via upload

--- index.js
+++ index.js
@@ -1,19 +1,20 @@
 var button = document.querySelectorAll("button");
+var audio = [ new Audio("sounds/crash.mp3") ,
+    new Audio("sounds/kick-bass.mp3"),
+    new Audio("sounds/snare.mp3"),
+    new Audio("sounds/tom-1.mp3"),
+    new Audio("sounds/tom-2.mp3"),
+    new Audio("sounds/tom-3.mp3"),
+    new Audio("sounds/tom-4.mp3")];
 
 
-for(var i=0;i< button.length ;i++) {
+
+for(let i=0;i< button.length ;i++) {
     button[i].addEventListener("click",z);
     function z(){
-        alert("i got clicked");
-    }
+
+            audio[i].play();
+
+        }
 };
 
-var i=0
-while (i< button.length){
-     button[i].addEventListener("click",z);
-    function z(){
-        alert("i got clicked");
-    }
-
-    i++
-};
